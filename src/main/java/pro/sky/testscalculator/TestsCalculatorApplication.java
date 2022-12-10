bacfd59296c9056fbf3d2b6d9e61cb78b7501bcf
letuu <albertvt@ya.ru>
2022-12-10 19:59:54 +0300

    Initial commit

---
+++ src/main/java/pro/sky/testscalculator/TestsCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.testscalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsCalculatorApplication.class, args);
+    }
+
+}
